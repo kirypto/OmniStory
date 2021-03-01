acd7f30ce8ded0740b2ef595bb903df3cbccfa11
kirypto <kirypto@gmail.com>
2021-02-28 18:01:00 -0600

    Change preferred string quote type to double

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  private _title = 'Timeline Tracker UI';
+  private _title = "Timeline Tracker UI";
 
   get title(): string {
     return this._title;
