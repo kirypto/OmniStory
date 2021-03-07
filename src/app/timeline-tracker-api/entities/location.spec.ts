2fce33467410466d6240fdab63dc32d49bcb06cd
kirypto <kirypto@gmail.com>
2021-03-06 23:44:48 -0600

    Create Location object

---
+++ src/app/timeline-tracker-api/entities/location.spec.ts
@@ -0,0 +1,44 @@
+import {Location} from "./location";
+
+describe("Location", () => {
+    const locationData = {
+        id: "location-00000000-0000-4000-8000-000000000000",
+        name: "a name",
+        description: "a description",
+        span: {
+            latitude: {low: 11034.738, high: 11066.318},
+            longitude: {low: 5457.91, high: 5483.174},
+            altitude: {low: 0.972, high: 1.034},
+            continuum: {low: -9383.0, high: Infinity},
+            reality: {low: 0, high: 0},
+        },
+        tags: ["tag1"],
+        metadata: {meta_key: "meta_val"}
+    };
+
+    describe("constructor", () => {
+        it("should create an instance of Location class", () => {
+            // Arrange
+            // Act
+            const actual = new Location(locationData);
+
+            // Assert
+            expect(actual).toBeTruthy();
+            expect(actual).toBeInstanceOf(Location);
+        });
+
+        it("should initialize properties", () => {
+            // Arrange
+            // Act
+            const actual = new Location(locationData);
+
+            // Assert
+            expect(actual.id).toEqual(locationData.id);
+            expect(actual.name).toEqual(locationData.name);
+            expect(actual.description).toEqual(locationData.description);
+            expect(actual.span).toEqual(locationData.span);
+            expect(actual.tags).toEqual(locationData.tags);
+            expect(actual.metadata).toEqual(locationData.metadata);
+        });
+    });
+});
