ab81f56ba118f83a4f51b2596a4d031d07368d8e
kirypto <kirypto@gmail.com>
2021-03-08 12:48:46 -0600

    Created a JsonBareWordNumericSymbolTranslator class to handle +/-Infinity and NaN in json responses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpClientModule} from "@angular/common/http";
 
+import {ttapiHttpInterceptorProviders} from "./timeline-tracker-api/http-interceptors";
 import {AppComponent} from "./app.component";
 import {LocationComponent} from "./timeline-tracker-api/components/location/location.component";
 import {EntityFinderComponent} from "./timeline-tracker-api/components/entity-finder/entity-finder.component";
@@ -16,7 +17,9 @@ import {EntityFinderComponent} from "./timeline-tracker-api/components/entity-fi
         BrowserModule,
         HttpClientModule,
     ],
-    providers: [],
+    providers: [
+        ttapiHttpInterceptorProviders
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
