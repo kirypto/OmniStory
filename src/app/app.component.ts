1871f5de6fa1cc03af91abc77d63c499012180d1
kirypto <kirypto@gmail.com>
2021-03-09 21:38:14 -0600

    Temporarily display location component in main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, Injectable, OnInit} from "@angular/core";
 import {Title} from "@angular/platform-browser";
+import {Location} from "./timeline-tracker-api/entities/location";
 
 @Component({
     selector: "app-root",
@@ -21,5 +22,22 @@ export class AppComponent implements OnInit {
 
     get title(): string {
         return this._title;
+    }
+
+    get location(): Location {
+        return new Location({
+            id: "location-00000000-0000-4000-8000-000000000000",
+            name: "a name",
+            description: "a description",
+            span: {
+                latitude: {low: 11034.738, high: 11066.318},
+                longitude: {low: 5457.91, high: 5483.174},
+                altitude: {low: 0.972, high: 1.034},
+                continuum: {low: -9383.0, high: Infinity},
+                reality: {low: 0, high: 0},
+            },
+            tags: ["tag1"],
+            metadata: new Map(Object.entries({meta_key: "meta_val"}))
+        });
     }
 }
