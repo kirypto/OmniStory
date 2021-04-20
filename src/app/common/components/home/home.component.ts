18753b885193cbf4bb33de9f63f2a56906649949
Garrett Hansen <kirypto@gmail.com>
2021-04-20 15:41:43 -0600

    Fix width and height of Home component to fill app content area

--- src/app/common/components/home/home.component.ts
+++ src/app/common/components/home/home.component.ts
@@ -4,7 +4,7 @@ import {RoutingComponent} from "../RoutingComponent";
 @Component({
     selector: "app-home-page",
     templateUrl: "./home.component.html",
-    styleUrls: ["./home.component.css"]
+    styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent extends RoutingComponent implements OnInit {
 
