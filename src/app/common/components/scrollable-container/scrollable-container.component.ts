a69926c56c97a18618e171ce28dcf331990f6402
Garrett Hansen <kirypto@gmail.com>
2021-04-30 12:53:33 -0600

    Add fxFlex input directives for all 4 margins to ScrollableContainer

--- src/app/common/components/scrollable-container/scrollable-container.component.ts
+++ src/app/common/components/scrollable-container/scrollable-container.component.ts
@@ -1,16 +1,54 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {isNumeric} from "rxjs/internal-compatibility";
+
 
 @Component({
     selector: "app-scrollable-container",
     templateUrl: "./scrollable-container.component.html",
     styleUrls: ["./scrollable-container.component.scss"]
 })
-export class ScrollableContainerComponent implements OnInit {
+export class ScrollableContainerComponent {
+    @Input() private _leftFxFlex: number | string = "0%";
+    @Input() private _rightFxFlex: number | string = "0%";
+    @Input() private _topFxFlex: number | string = "0%";
+    @Input() private _bottomFxFlex: number | string = "0%";
 
-    constructor() {
+    public constructor() {
     }
 
-    ngOnInit(): void {
+    public get LeftFxFlex(): string {
+        return convert(this._leftFxFlex);
     }
 
+    public get RightFxFlex(): string {
+        return convert(this._rightFxFlex);
+    }
+
+    public get TopFxFlex(): string {
+        return convert(this._topFxFlex);
+    }
+
+    public get BottomFxFlex(): string {
+        return convert(this._bottomFxFlex);
+    }
+
+}
+
+function convert(fxFlexInstruction: number | string): string {
+    let msg = `Converting '${fxFlexInstruction}'`;
+    let fxFlexInstructionStr: string;
+    if (typeof fxFlexInstruction === "string") {
+        fxFlexInstructionStr = fxFlexInstruction;
+    } else if (typeof fxFlexInstruction === "number") {
+        fxFlexInstructionStr = fxFlexInstruction.toString();
+        msg += `-> From number to '${fxFlexInstructionStr}'`;
+    } else {
+        throw new Error(`[${ScrollableContainerComponent}] Failed to convert flex instruction '${fxFlexInstruction}'`);
+    }
+    if (isNumeric(fxFlexInstructionStr)) {
+        fxFlexInstructionStr = `${fxFlexInstructionStr}%`;
+        msg += `-> From numeric string to '${fxFlexInstructionStr}'`;
+    }
+    console.log(msg);
+    return fxFlexInstructionStr;
 }
