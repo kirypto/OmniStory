0d99737b3632370b77a22ad966481bc812aa9263
Garrett Hansen <kirypto@gmail.com>
2021-05-04 16:51:27 -0600

    Revert attempt to use date picker

--- src/app/common/components/continuum-input/continuum-input.component.ts
+++ src/app/common/components/continuum-input/continuum-input.component.ts
@@ -1,56 +1,19 @@
-import {Component, Input} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {CalendarService} from "../../services/calendar/calendar.service";
-import {CalendarType} from "../../types/calendar-type";
 
 @Component({
     selector: "app-continuum-input",
     templateUrl: "./continuum-input.component.html",
     styleUrls: ["./continuum-input.component.scss"]
 })
-export class ContinuumInputComponent {
-    private _continuumLow = 0.0;
-    private _continuumHigh = 0.0;
+export class ContinuumInputComponent implements OnInit {
 
     public constructor(
         private _calendarService: CalendarService
     ) {
     }
 
-    public get ContinuumHigh(): number {
-        return this._continuumHigh;
+    public ngOnInit(): void {
     }
 
-    @Input()
-    public set ContinuumHigh(value: number) {
-        this._continuumHigh = value;
-        if (this._continuumLow > value) {
-            this._continuumLow = value;
-        }
-    }
-
-    public get ContinuumLow(): number {
-        return this._continuumLow;
-    }
-
-    @Input()
-    public set ContinuumLow(value: number) {
-        this._continuumLow = value;
-        if (this._continuumHigh < value) {
-            this._continuumHigh = value;
-        }
-    }
-
-    public get GregorianStart(): Date {
-        if (this._calendarService.System !== CalendarType.Gregorian) {
-            return null;
-        }
-        return this._calendarService.translateFromContinuum(this._continuumLow);
-    }
-
-    public get GregorianEnd(): Date {
-        if (this._calendarService.System !== CalendarType.Gregorian) {
-            return null;
-        }
-        return this._calendarService.translateFromContinuum(this._continuumHigh);
-    }
 }
