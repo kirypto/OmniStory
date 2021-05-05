72700b0fbfa6c94bd45daeae3cd42b4366b5fc52
Garrett Hansen <kirypto@gmail.com>
2021-05-04 22:26:22 -0600

    Tie in Calendar Service to support inputs for each component of a Gregorian date

--- src/app/common/components/continuum-input/continuum-input.component.ts
+++ src/app/common/components/continuum-input/continuum-input.component.ts
@@ -1,19 +1,35 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from "@angular/core";
+
 import {CalendarService} from "../../services/calendar/calendar.service";
+import {CalendarPart} from "../../types/calendar-type";
 
 @Component({
     selector: "app-continuum-input",
     templateUrl: "./continuum-input.component.html",
     styleUrls: ["./continuum-input.component.scss"]
 })
-export class ContinuumInputComponent implements OnInit {
+export class ContinuumInputComponent implements OnChanges {
+    public continuumParts: CalendarPart[] = [];
+
+    @Input() public continuum: number;
+    @Output() public continuumChange = new EventEmitter<number>();
 
     public constructor(
         private _calendarService: CalendarService
     ) {
     }
 
-    public ngOnInit(): void {
+    public OnChange(): void {
+        this.continuum = this._calendarService.translateToContinuum(this.continuumParts);
+        this.continuumChange.emit(this.continuum);
     }
 
+    public ngOnChanges(changes: SimpleChanges): void {
+        this.continuumParts = this._calendarService.translateFromContinuum(this.continuum);
+    }
+
+    public continuumPartTrackBy(index: number, _continuumPart: CalendarPart): number {
+        // Identify using the index so that Angular does not re-render mid typing causing focus loss after each character change
+        return index;
+    }
 }
--- src/app/common/components/world-calendar/world-calendar.component.ts
+++ src/app/common/components/world-calendar/world-calendar.component.ts
@@ -1,21 +1,14 @@
-import {Component, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
     selector: "app-world-calendar",
     templateUrl: "./world-calendar.component.html",
     styleUrls: ["./world-calendar.component.scss"]
 })
-export class WorldCalendarComponent implements OnInit {
+export class WorldCalendarComponent {
     ContinuumLow = 0;
 
     constructor() {
-    }
-
-    ngOnInit(): void {
-        setInterval(() => {
-            this.ContinuumLow += 1;
-            console.log(this.ContinuumLow);
-        }, 2000);
     }
 
 }
