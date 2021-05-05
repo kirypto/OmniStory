05bc9f6c3193b1b71d9e2ad98bb62ed8dcf23fff
Garrett Hansen <kirypto@gmail.com>
2021-05-05 12:56:27 -0600

    Add a lower and higher continuum input to World Calendar, linking to ensure lower is less or equal to higher

--- src/app/common/components/world-calendar/world-calendar.component.ts
+++ src/app/common/components/world-calendar/world-calendar.component.ts
@@ -6,9 +6,27 @@ import {Component} from "@angular/core";
     styleUrls: ["./world-calendar.component.scss"]
 })
 export class WorldCalendarComponent {
-    ContinuumLow = 0;
+    private _continuumLow = 0;
+    private _continuumHigh = 0;
 
-    constructor() {
+    public constructor() {
     }
 
+    get continuumLow(): number {
+        return this._continuumLow;
+    }
+
+    set continuumLow(value: number) {
+        this._continuumLow = value;
+        this._continuumHigh = Math.max(this._continuumHigh, value);
+    }
+
+    get continuumHigh(): number {
+        return this._continuumHigh;
+    }
+
+    set continuumHigh(value: number) {
+        this._continuumHigh = value;
+        this._continuumLow = Math.min(this._continuumLow, value);
+    }
 }
