da574767f42b0a6f404c93fe964549158c0f2089
kirypto <kirypto@gmail.com>
2022-05-20 13:52:55 -0600

    Modify home page to show logged in account (#10)

--- src/app/common/components/home/home.component.ts
+++ src/app/common/components/home/home.component.ts
@@ -1,15 +1,21 @@
 import {Component, OnInit} from "@angular/core";
 import {RoutingComponent} from "../RoutingComponent";
+import {AuthService, User} from "@auth0/auth0-angular";
+import {Observable} from "rxjs";
 
 @Component({
     selector: "app-home-page",
     templateUrl: "./home.component.html",
-    styleUrls: ["./home.component.scss"]
+    styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent extends RoutingComponent implements OnInit {
 
-    public constructor() {
+    public constructor(private _authService: AuthService) {
         super();
+    }
+
+    public get user(): Observable<User | null | undefined> {
+        return this._authService.user$;
     }
 
     public ngOnInit(): void {
