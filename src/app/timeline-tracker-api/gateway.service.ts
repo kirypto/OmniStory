50078f75650e572aa6f6a7d3083a7b0450219b4c
kirypto <kirypto@gmail.com>
2022-05-20 18:14:30 -0600

    Tied in auth token retrieval before calling ttapi (#10)

--- src/app/timeline-tracker-api/gateway.service.ts
+++ src/app/timeline-tracker-api/gateway.service.ts
@@ -1,11 +1,13 @@
 import {Injectable} from "@angular/core";
-import {Fetcher} from "openapi-typescript-fetch";
+import {Fetcher, Middleware} from "openapi-typescript-fetch";
 
 import {AppConfigService} from "../common/services/app-config/app-config.service";
 import {paths} from "./schema";
-import {from, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {handleError} from "./services/util";
-import {catchError, tap, map, filter} from "rxjs/operators";
+import {catchError, map, switchMap} from "rxjs/operators";
+import {AuthService} from "@auth0/auth0-angular";
+import {CustomRequestInit, Fetch} from "openapi-typescript-fetch/dist/cjs/types";
 
 @Injectable({
     providedIn: "root",
@@ -13,9 +15,10 @@ import {catchError, tap, map, filter} from "rxjs/operators";
 export class GatewayService {
     private _fetcher;
 
-    private _getWorlds;
+    private readonly _getWorlds;
 
     constructor(
+        private _authService: AuthService,
         appConfigService: AppConfigService,
     ) {
         this._fetcher = Fetcher.for<paths>();
@@ -24,24 +27,33 @@ export class GatewayService {
             init: {
                 headers: {},
             },
-            use: [],
+            use: [
+                this.authenticationMiddleware,
+            ],
         });
 
         this._getWorlds = this._fetcher.path("/worlds").method("get").create();
     }
 
-
     public retrieveWorldIds(): Observable<string[]> {
-        return from((async () => {
-            const {status, data: pets} = await this._getWorlds();
-            console.log(`Fetched ${pets}, status code ${status}`);
-            return [status, pets];
-        })()).pipe(
-            tap(([statusCode, locationDataArr]) => console.log(`Fetched ${locationDataArr.length} Location Ids`)),
-            // filter((statusCode, value) => statusCode === 200),
-            map(([statusCode, value]) => value),
-            catchError(handleError<string[]>("retrieveLocationIds()", [])),
+        return this.getAuthToken().pipe(
+            switchMap(async authToken => {
+                const {statusCode, data: ids} = await this._getWorlds() as { statusCode: number, data: string[] };
+                console.log(`Fetched ${ids}, statusCode code ${statusCode}`);
+                return ids;
+            }),
+            catchError(handleError<string[]>("retrieveWorldIds()", [])),
+            // map(([statusCode: number, ids: string[]]) => ids),
         );
+        // return from((async () => {
+        //     const {status, data: pets} = await this._getWorlds();
+        //     console.log(`Fetched ${pets}, status code ${status}`);
+        //     return [status, pets];
+        // })()).pipe(
+        //     tap(([statusCode, locationDataArr]) => console.log(`Fetched ${locationDataArr.length} Location Ids`)),
+        //     // filter((statusCode, value) => statusCode === 200),
+        //     map(([statusCode, value]) => value),
+        // );
         // await this._getWorlds();
         // const url = `${this._timelineTrackerApiUrl}/locations${constructEncodedQueryParams(filters)}`;
         // return this._httpClient.get<string[]>(url)
@@ -49,5 +61,23 @@ export class GatewayService {
         //         tap(locationDataArr => console.log(`Fetched ${locationDataArr.length} Location Ids`)),
         //         catchError(handleError<string[]>("retrieveLocationIds()", [])),
         //     );
+    }
+
+    private getAuthToken(): Observable<string> {
+        return this._authService.getIdTokenClaims().pipe(
+            map(value => value.__raw),
+        );
+        // return from((async () => {
+        //     const claims = await this._authService.getIdTokenClaims();
+        //     return claims;
+        // })());
+    }
+
+    private authenticationMiddleware: Middleware = async (url: string, init: CustomRequestInit, next: Fetch) => {
+        init.headers.set("Authorization", `Bearer ${await this.getAuthToken().toPromise()}`);
+        console.log(`fetching ${url} with headers ${init.headers}`);
+        const response = await next(url, init);
+        console.log(`fetched ${url}`);
+        return response;
     }
 }
