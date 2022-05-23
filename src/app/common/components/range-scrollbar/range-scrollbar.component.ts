eefcb6592d8aca83938365965779619edc573fae
kirypto <kirypto@gmail.com>
2022-05-23 16:11:43 -0600

    Add main handle to range scrollbar

--- src/app/common/components/range-scrollbar/range-scrollbar.component.ts
+++ src/app/common/components/range-scrollbar/range-scrollbar.component.ts
@@ -15,6 +15,7 @@ export class RangeScrollbarComponent implements OnInit, AfterViewInit {
 
     @ViewChild("draggableArea") private _draggableAreaElement: ElementRef;
     @ViewChild("minHandle") private _minHandleElement: ElementRef;
+    @ViewChild("mainHandle") private _mainHandleElement: ElementRef;
     @ViewChild("maxHandle") private _maxHandleElement: ElementRef;
     private readonly _allowedDirections = new Set(["vertical", "horizontal"]);
     private readonly _allowedDirectionStrings = [...this._allowedDirections].join(", ");
@@ -38,12 +39,19 @@ export class RangeScrollbarComponent implements OnInit, AfterViewInit {
 
     public ngAfterViewInit(): void {
         if (!this.isVertical) {
-            // Initial position stacks vertically, move it to be to the right instead of below
-            this._maxHandleElement.nativeElement.style.transform = "translate3d(10px, -100%, 0px)";
+            // Initial position stacks vertically, main and max handles to be to the right of the min instead of below
+            this._mainHandleElement.nativeElement.style.transform = "translate3d(10px, -100%, 0px)";
+            this._maxHandleElement.nativeElement.style.transform = "translate3d(20px, -200%, 0px)";
         }
     }
 
     public onDragMin($event: CdkDragMove): void {
+        const pixelPosition = {x: $event.distance.x, y: $event.distance.y};
+        const percentPosition = this.calcPercentagePosition(pixelPosition);
+        console.log(`Percentage position: x=${percentPosition.x}, y=${percentPosition.y}`);
+    }
+
+    public onDragMain($event: CdkDragMove): void {
         const pixelPosition = {x: $event.distance.x, y: $event.distance.y};
         const percentPosition = this.calcPercentagePosition(pixelPosition);
         console.log(`Percentage position: x=${percentPosition.x}, y=${percentPosition.y}`);
