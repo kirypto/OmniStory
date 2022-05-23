7ff7a6fc28e26162c2ed6eb20f909d3f87e355be
kirypto <kirypto@gmail.com>
2022-05-23 14:43:51 -0600

    Got steel thread percentage position calculation working when handles are dragged

--- src/app/common/components/range-scrollbar/range-scrollbar.component.ts
+++ src/app/common/components/range-scrollbar/range-scrollbar.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, ElementRef, Input, OnInit, ViewChild} from "@angular/core";
 import {Required} from "../../util";
+import {CdkDragMove} from "@angular/cdk/drag-drop";
 
 @Component({
     selector: "app-range-scrollbar",
@@ -9,6 +10,7 @@ import {Required} from "../../util";
 export class RangeScrollbarComponent implements OnInit {
     @Input() @Required public scrollDirection: string;
 
+    @ViewChild("draggableArea") draggableAreaElement: ElementRef;
     private readonly _allowedDirections = new Set(["vertical", "horizontal"]);
     private readonly _allowedDirectionStrings = [...this._allowedDirections].join(", ");
 
@@ -23,11 +25,28 @@ export class RangeScrollbarComponent implements OnInit {
         return this.scrollDirection === "vertical";
     }
 
-
     public ngOnInit(): void {
         if (!this._allowedDirections.has(this.scrollDirection)) {
             throw new Error(`Input 'scrollDirection' must be one of: ${this._allowedDirectionStrings}; was '${this.scrollDirection}'.`);
         }
     }
 
+    public onDrag($event: CdkDragMove): void {
+        const pixelPosition = {x: $event.distance.x, y: $event.distance.y};
+        const percentPosition = this.calcPercentagePosition(pixelPosition);
+        console.log(`Percentage position: x=${percentPosition.x}, y=${percentPosition.y}`);
+    }
+
+    private get draggableAreaSize(): { width: number, height: number } {
+        const nativeElement = this.draggableAreaElement.nativeElement;
+        return {width: nativeElement.offsetWidth, height: nativeElement.offsetHeight};
+    }
+
+    private calcPercentagePosition(pixelPosition: { x: number, y: number }): { x: number, y: number } {
+        const draggableAreaSize = this.draggableAreaSize;
+        return {
+            x: pixelPosition.x / draggableAreaSize.width,
+            y: pixelPosition.y / draggableAreaSize.height,
+        };
+    }
 }
