b2f8f129bf38923ec41266ef06a47e94d21cca55
kirypto <kirypto@gmail.com>
2022-05-23 16:50:51 -0600

    Implementing a function to update the handle positions based on an absolute range and selection range*
    - Currently only operates for vertical scrollbar

--- src/app/common/components/range-scrollbar/range-scrollbar.component.ts
+++ src/app/common/components/range-scrollbar/range-scrollbar.component.ts
@@ -20,6 +20,12 @@ export class RangeScrollbarComponent implements OnInit, AfterViewInit {
     private readonly _allowedDirections = new Set(["vertical", "horizontal"]);
     private readonly _allowedDirectionStrings = [...this._allowedDirections].join(", ");
 
+    private _minimum = 0.0;
+    private _maximum = 100.0;
+
+    private _selectionLow = 25;
+    private _selectionHigh = 75;
+
     public constructor() {
     }
 
@@ -43,6 +49,8 @@ export class RangeScrollbarComponent implements OnInit, AfterViewInit {
             this._mainHandleElement.nativeElement.style.transform = "translate3d(10px, -100%, 0px)";
             this._maxHandleElement.nativeElement.style.transform = "translate3d(20px, -200%, 0px)";
         }
+
+        this.updateHandlePositions();
     }
 
     public onDragMin($event: CdkDragMove): void {
@@ -66,6 +74,33 @@ export class RangeScrollbarComponent implements OnInit, AfterViewInit {
     private get draggableAreaSize(): { width: number, height: number } {
         const nativeElement = this._draggableAreaElement.nativeElement;
         return {width: nativeElement.offsetWidth, height: nativeElement.offsetHeight};
+    }
+
+    private updateHandlePositions(): void {
+        const selectionLowPercent = this.calcSelectionPercentage(this._selectionLow);
+        const selectionHighPercent = this.calcSelectionPercentage(this._selectionHigh);
+        const draggableAreaSize = this.draggableAreaSize;
+
+        let pixelPositionLow: number;
+        let pixelPositionHigh: number;
+        if (this.isVertical) {
+            pixelPositionLow = draggableAreaSize.height * selectionLowPercent;
+            pixelPositionHigh = draggableAreaSize.height * selectionHighPercent;
+            const pixelPositionMid = (pixelPositionLow + pixelPositionHigh) / 2;
+
+            this._minHandleElement.nativeElement.style.transform = `translate3d(0px, ${pixelPositionLow}px, 0px)`;
+            this._mainHandleElement.nativeElement.style.transform = `translate3d(0px, ${pixelPositionMid - 10}px, 0px)`;
+            this._maxHandleElement.nativeElement.style.transform = `translate3d(0px, ${pixelPositionHigh - 20}px, 0px)`;
+        } else {
+            pixelPositionLow = draggableAreaSize.width * selectionLowPercent;
+            pixelPositionHigh = draggableAreaSize.width * selectionHighPercent;
+            console.log(`${pixelPositionLow} ${pixelPositionHigh}`);
+        }
+    }
+
+    private calcSelectionPercentage(selectionPosition: number): number {
+        const selectionPositionPercent = (selectionPosition - this._minimum) / (this._maximum - this._minimum);
+        return Math.max(this._minimum, Math.min(this._maximum, selectionPositionPercent));
     }
 
     private calcPercentagePosition(pixelPosition: Position): Position {
