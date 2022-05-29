a10aad4f6eebc41ce51acfb8d555e8a660862b31
kirypto <kirypto@gmail.com>
2022-05-29 11:56:46 -0600

    Move deepCopy to util.ts

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -40,3 +40,40 @@ export function handleError<T>(operation = "operation", result?: T): (error: any
     };
 }
 
+export function deepCopy<T>(obj: T): T {
+    let copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null === obj || undefined === obj || "object" !== typeof obj) {
+        return obj;
+    }
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (let i = 0, len = obj.length; i < len; i++) {
+            copy[i] = deepCopy(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (const attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+                copy[attr] = deepCopy(obj[attr]);
+            }
+        }
+        return copy;
+    }
+
+    throw new Error(`Unable to copy obj! Its type '${typeof obj}' isn't supported.`);
+}
--- src/app/test-helpers.spec.ts
+++ src/app/test-helpers.spec.ts
@@ -11,40 +11,6 @@ import {sampleApplicationConfig} from "./common/services/app-config/app-config.s
 describe("Workaround to have this considered as a test only file", () => {
 });
 
-export function deepCopy<T>(obj: T): T {
-    let copy;
-
-    // Handle the 3 simple types, and null or undefined
-    if (null === obj || undefined === obj || "object" !== typeof obj) { return obj; }
-
-    // Handle Date
-    if (obj instanceof Date) {
-        copy = new Date();
-        copy.setTime(obj.getTime());
-        return copy;
-    }
-
-    // Handle Array
-    if (obj instanceof Array) {
-        copy = [];
-        for (let i = 0, len = obj.length; i < len; i++) {
-            copy[i] = deepCopy(obj[i]);
-        }
-        return copy;
-    }
-
-    // Handle Object
-    if (obj instanceof Object) {
-        copy = {};
-        for (const attr in obj) {
-            if (obj.hasOwnProperty(attr)) { copy[attr] = deepCopy(obj[attr]); }
-        }
-        return copy;
-    }
-
-    throw new Error(`Unable to copy obj! Its type '${typeof obj}' isn't supported.`);
-}
-
 
 export const testingModuleDefinitions = {
     providers: getTestProviders(),
