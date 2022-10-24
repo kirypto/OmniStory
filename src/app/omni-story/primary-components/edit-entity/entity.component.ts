dd1ae022105e113019ebdad87e4ba6ec643f9b84
kirypto <kirypto@gmail.com>
2022-10-24 14:47:37 -0600

    Rework Entity component to handle viewing any entity type

--- src/app/omni-story/primary-components/edit-entity/entity.component.ts
+++ src/app/omni-story/primary-components/edit-entity/entity.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
-import {Location, LocationId, WorldId} from "../../../timeline-tracker-api/ttapi-types";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Entity, EntityId, Journey, Span, WorldId} from "../../../timeline-tracker-api/ttapi-types";
 import {TtapiGatewayService} from "../../../timeline-tracker-api/ttapi-gateway.service";
 import {SubscribingComponent} from "../../../common/components/SubscribingComponent";
+import {Observable} from "rxjs";
 
 @Component({
     selector: "app-entity",
@@ -11,14 +12,21 @@ import {SubscribingComponent} from "../../../common/components/SubscribingCompon
 })
 export class EntityComponent extends SubscribingComponent implements OnInit {
     private _worldId: WorldId;
-    private _entityId: LocationId;
-    private _entity: Location;
+    private _entityId: EntityId;
+    private _entity: Entity;
 
     public constructor(
         private _route: ActivatedRoute,
         private _ttapiGateway: TtapiGatewayService,
+        private router: Router,
     ) {
         super();
+        // force route reload whenever params change;
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    }
+
+    public get isLoaded(): boolean {
+        return this._entity !== undefined;
     }
 
     public get worldId(): string {
@@ -33,44 +41,75 @@ export class EntityComponent extends SubscribingComponent implements OnInit {
         return this._entity?.name || "";
     }
 
-    public get entityData(): string {
-        if (!this._entity) {
-            return "";
+    public set name(value: string) {
+        if (this.isLoaded) {
+            this._entity.name = value;
         }
-        return JSON.stringify(this._entity, null, 4);
     }
 
-    public get entity(): Location {
-        return this._entity;
+    public get description(): string {
+        return this._entity?.description || "";
     }
 
-    public get span(): string {
-        return this._entity ? JSON.stringify(this._entity.span, null, 4) : "";
-    }
-
-    public set span(value: string) {
-        if (this._entity) {
-            this._entity.span = JSON.parse(value);
+    public set description(value: string) {
+        if (this.isLoaded) {
+            this._entity.description = value;
         }
     }
 
     public get tags(): string {
-        return this._entity ? JSON.stringify(this._entity.tags, null, 4) : "";
+        return this.isLoaded ? JSON.stringify(this._entity.tags, null, 4) : "";
     }
 
     public set tags(value: string) {
-        if (this._entity) {
+        if (this.isLoaded) {
             this._entity.tags = JSON.parse(value);
         }
     }
 
     public get attributes(): string {
-        return this._entity ? JSON.stringify(this._entity.attributes, null, 4) : "";
+        return this.isLoaded ? JSON.stringify(this._entity.attributes, null, 4) : "";
     }
 
     public set attributes(value: string) {
-        if (this._entity) {
+        if (this.isLoaded) {
             this._entity.attributes = JSON.parse(value);
+        }
+    }
+
+    public get span(): string {
+        // Only applicable for Location and Event
+        const asSpanningEntity = this._entity as { span: Span };
+        let span = "";
+        if (this.isLoaded && asSpanningEntity?.span) {
+            span = JSON.stringify(asSpanningEntity.span, null, 4);
+        }
+        return span;
+    }
+
+    public set span(value: string) {
+        // Only applicable for Location and Event
+        const asSpanningEntity = this._entity as { span: Span };
+        if (this.isLoaded && asSpanningEntity?.span) {
+            asSpanningEntity.span = JSON.parse(value);
+        }
+    }
+
+    public get journey(): string {
+        // Only applicable for Traveler
+        const asJourneyingEntity = this._entity as { journey: Journey };
+        let journey = "";
+        if (this.isLoaded && asJourneyingEntity?.journey) {
+            journey = JSON.stringify(asJourneyingEntity.journey, null, 4);
+        }
+        return journey;
+    }
+
+    public set journey(value: string) {
+        // Only applicable for Traveler
+        const asJourneyingEntity = this._entity as { journey: Journey };
+        if (this.isLoaded && asJourneyingEntity?.journey) {
+            asJourneyingEntity.journey = JSON.parse(value);
         }
     }
 
@@ -79,17 +118,31 @@ export class EntityComponent extends SubscribingComponent implements OnInit {
         this._worldId = paramMap.get("worldId");
         this._entityId = paramMap.get("entityId");
         console.log(`Editing ${this._worldId} -> ${this._entityId}`);
-        // const entityType = this._entityId.split("-", 1)[0];
-        //
-        // switch (entityType) {
-        //     case "location":
-        //         const
-        //         break;
-        // }
 
-        this.newSubscription = this._ttapiGateway.fetch("/api/world/{worldId}/location/{locationId}", "get", {
-            worldId: this._worldId,
-            locationId: this._entityId,
-        }).subscribe((value: Location) => this._entity = value);
+
+        let fetchObservable: Observable<Entity>;
+        if (this._entityId.startsWith("world")) {
+            fetchObservable = this._ttapiGateway.fetch("/api/world/{worldId}", "get", {
+                worldId: this._worldId,
+            });
+        } else if (this._entityId.startsWith("location")) {
+            fetchObservable = this._ttapiGateway.fetch("/api/world/{worldId}/location/{locationId}", "get", {
+                worldId: this._worldId,
+                locationId: this._entityId,
+            });
+        } else if (this._entityId.startsWith("traveler")) {
+            fetchObservable = this._ttapiGateway.fetch("/api/world/{worldId}/traveler/{travelerId}", "get", {
+                worldId: this._worldId,
+                travelerId: this._entityId,
+            });
+        } else if (this._entityId.startsWith("event")) {
+            fetchObservable = this._ttapiGateway.fetch("/api/world/{worldId}/event/{eventId}", "get", {
+                worldId: this._worldId,
+                eventId: this._entityId,
+            });
+        } else {
+            throw new Error(`Cannot edit entity '${this._entityId}', unknown entity type`);
+        }
+        this.newSubscription = fetchObservable.subscribe((value: Entity) => this._entity = value);
     }
 }
