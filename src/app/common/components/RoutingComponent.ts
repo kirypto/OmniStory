26ade4a2c3962062c94ecfa1e49f769aaf045f88
kirypto <kirypto@gmail.com>
2022-10-30 19:29:10 -0600

    Add side panel to home page to display existing worlds

--- src/app/common/components/RoutingComponent.ts
+++ src/app/common/components/RoutingComponent.ts
@@ -1,7 +1,23 @@
 import {RoutePaths} from "../types/route-paths";
+import {SubscribingComponent} from "./SubscribingComponent";
+import {EntityId, WorldId} from "../../timeline-tracker-api/ttapi-types";
 
-export abstract class RoutingComponent {
-   public get RoutePaths(): typeof RoutePaths {
-      return RoutePaths;
-   }
+export abstract class RoutingComponent extends SubscribingComponent {
+    public get RoutePaths(): typeof RoutePaths {
+        return RoutePaths;
+    }
+
+    public constructPath(path: RoutePaths, replacements: {
+        worldId?: WorldId,
+        entityId?: EntityId,
+    }): string {
+        let resultPath = path as string;
+        if (replacements.worldId) {
+            resultPath = resultPath.replace(":worldId", replacements.worldId);
+        }
+        if (replacements.entityId) {
+            resultPath = resultPath.replace(":entityId", replacements.entityId);
+        }
+        return resultPath;
+    }
 }
--- src/app/omni-story/primary-components/home/home.component.ts
+++ src/app/omni-story/primary-components/home/home.component.ts
@@ -1,20 +1,44 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {RoutingComponent} from "../../../common/components/RoutingComponent";
 import {AuthService, User} from "@auth0/auth0-angular";
-import {Observable} from "rxjs";
+import {mergeMap, Observable} from "rxjs";
+import {TtapiGatewayService} from "../../../timeline-tracker-api/ttapi-gateway.service";
+import {World, WorldId, WorldIds} from "../../../timeline-tracker-api/ttapi-types";
+import {Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
     selector: "app-home-page",
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent extends RoutingComponent {
+export class HomeComponent extends RoutingComponent implements OnInit {
+    private _worlds: Map<WorldId, World> = new Map<WorldId, World>();
 
-    public constructor(private _authService: AuthService) {
+    public constructor(
+        private _authService: AuthService,
+        private _ttapiGateway: TtapiGatewayService,
+        private _router: Router,
+    ) {
         super();
     }
 
     public get user(): Observable<User | null | undefined> {
         return this._authService.user$;
+    }
+
+    public get worlds(): Map<WorldId, World> {
+        return this._worlds;
+    }
+
+    public ngOnInit(): void {
+        this.user.pipe(
+            filter((user: User | null | undefined) => user !== null && user !== undefined),
+            mergeMap(() => this._ttapiGateway.fetch("/api/worlds", "get", {})),
+            mergeMap((worldIds: WorldIds) => worldIds),
+            mergeMap((worldId: WorldId) => this._ttapiGateway.fetch("/api/world/{worldId}", "get", {
+                worldId,
+            })),
+        ).subscribe((world: World) => this._worlds.set(world.id, world));
     }
 }
