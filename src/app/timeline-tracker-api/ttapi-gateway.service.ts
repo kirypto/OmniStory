021596fed6b46e76485070c2ab4383f58d17311f
kirypto <kirypto@gmail.com>
2022-11-02 20:40:33 -0600

    Add logic to automatically login when TtapiGateway is invoked without being logged in

--- src/app/timeline-tracker-api/ttapi-gateway.service.ts
+++ src/app/timeline-tracker-api/ttapi-gateway.service.ts
@@ -3,11 +3,15 @@ import {Fetcher, Middleware, OpArgType, OpReturnType} from "openapi-typescript-f
 
 import {AppConfigService} from "../common/services/app-config/app-config.service";
 import {paths} from "./ttapi-schema";
-import {firstValueFrom, from, Observable} from "rxjs";
+import {catchError, EMPTY, firstValueFrom, from, Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {AuthService} from "@auth0/auth0-angular";
 import {CustomRequestInit, Fetch} from "openapi-typescript-fetch/dist/cjs/types";
 import {WorldIds} from "./ttapi-types";
+import {Router} from "@angular/router";
+
+class NotLoggedInError extends Error {
+}
 
 @Injectable({
     providedIn: "root",
@@ -19,6 +23,7 @@ export class TtapiGatewayService {
 
     constructor(
         private _authService: AuthService,
+        private _router: Router,
         appConfigService: AppConfigService,
     ) {
         const addAuthenticationTokenMiddleware: Middleware = async (url: string, init: CustomRequestInit, next: Fetch) => {
@@ -46,19 +51,31 @@ export class TtapiGatewayService {
         path: TPath,
         method: TMethod,
         args: TArgs,
+        redirectToAuth: boolean = true,
     ): Observable<TReturn> {
         return from((async () => {
             const fetcher = this._fetcher.path(path).method(method).create();
             const {data} = await fetcher(args);
             return data as TReturn;
-        })());
+        })()).pipe(
+            catchError((err) => {
+                if (err instanceof NotLoggedInError && redirectToAuth) {
+                    console.log("Not logged in, redirecting to auth");
+                    this._authService.loginWithRedirect({
+                        appState: {target: this._router.url},
+                    });
+                    return EMPTY;
+                }
+                throw err;
+            }),
+        );
     }
 
     private getAuthToken(): Observable<string> {
         return this._authService.getIdTokenClaims().pipe(
             map(value => {
                 if (value === null || value === undefined) {
-                    throw new Error("Not logged in!");
+                    throw new NotLoggedInError("Not logged in!");
                 }
                 return value.__raw;
             }),
